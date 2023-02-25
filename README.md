6b7a3f3064a2f77e91a38bcfb5b00f2d50995e04
adolfoiracheta <84049381+adolfoiracheta@users.noreply.github.com>
2023-02-25 01:33:20 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arrays-MyStudies-True-or-False-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-8dtwmu)
---
+++ index.js
@@ -0,0 +1,31 @@
+let myStudies = ['Animating in Maya', 'Modeling in Maya', 'Editing Tips & Tricks', 'Draw Comic Characters'];
+
+let favoriteStudy = myStudies[0];
+
+if (myStudies[0] == favoriteStudy) {   
+  console.log(myStudies[0] + ' ' + 'True');
+} 
+else {
+  console.log(myStudies[0] + ' ' + 'False');
+}
+
+if (myStudies[1] == favoriteStudy) {   
+  console.log(myStudies[1] + ' ' + 'True');
+} 
+else {
+  console.log(myStudies[1] + ' ' + 'False');
+}
+
+if (myStudies[2] == favoriteStudy) {   
+  console.log(myStudies[2] + ' ' + 'True');
+} 
+else {
+  console.log(myStudies[2] + ' ' + 'False');
+}
+
+if (myStudies[3] == favoriteStudy) {   
+  console.log(myStudies[3] + ' ' + 'True');
+} 
+else {
+  console.log(myStudies[3] + ' ' + 'False');
+}
